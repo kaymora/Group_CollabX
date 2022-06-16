1cc0ea79895e8200b1c79ab3c491c0861f3fd3d1
dilshoddostiev <dilshoddostiev@hotmail.com>
2022-06-15 23:56:48 -0500

    My first 1st Class.

---
+++ src/Dilshod/FirstClass.java
@@ -0,0 +1,13 @@
+package Dilshod;
+
+public class FirstClass<hood, baby> {
+
+  //  Yall we doing good in the hood
+
+   // 1. Karina good luck with baby
+
+    // 2. Khosiyqt be positive
+
+    //3. Selim enjoy your games
+
+}
